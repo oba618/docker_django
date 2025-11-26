ee1f7c50cf733c98de11756acd41d2c66abd27e8
oba618 <tomohiro900618@gmail.com>
2025-11-27 08:21:39 +0900

    Update index Archives

--- src/djangotutorial/index/views.py
+++ src/djangotutorial/index/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Content
+
 
 def index(request):
-    return render(request, "index/index.html")
+    latest_content_list = Content.objects.order_by("-pub_date")[:10]
+    context = {"latest_content_list": latest_content_list}
+    return render(request, "index/index.html", context)
